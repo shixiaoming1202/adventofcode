14abff69c35fc5e40b599ad0ed872b13b0b63bcf
Alex S <shixiaoming1202@users.noreply.github.com>
2018-12-06 15:25:14 -0500

    Adding day 5 solutions

--- 2018/d5.js
+++ 2018/d5.js
@@ -1,1 +1,35 @@
+var input = document.querySelector("pre").textContent.trim().split("");
 
+function disapear (a = "", b = "") {
+    return a!==b && a.toLowerCase() === b.toLowerCase();
+}
+
+// Q1
+function rmv (list) {
+    do {
+        var len = list.length;
+
+        list.forEach((item, idx, arr) => {
+            if (disapear(item, arr[idx+1])) {
+                arr[idx] = arr[idx+1] = "";
+            };
+        });
+
+        list = list.join("").split("");
+    } while (len !== list.length)
+
+    return len;
+}
+
+//Q2
+
+function fnd () {
+    var result = [];
+
+    "abcdefghijklmnopqrstuvwxyz".split("").forEach(cha => {
+        var res = input.filter(i => (i !== cha && i !== cha.toUpperCase()));
+        result.push(rmv(res));
+    });
+
+    return Math.min(...result);
+}
