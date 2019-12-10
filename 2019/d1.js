f05a241f46fac3831ece7bac8929fe467d321f72
Alex Shi <alexshisuper@gmail.com>
2019-12-09 21:34:03 -0500

    d1 question two

--- 2019/d1.js
+++ 2019/d1.js
@@ -5,13 +5,32 @@ class Solution {
 		return Math.floor(Number(mass) / 3) - 2;
 	}
 
+	getAdditionalFuel(mass) {
+		let fuel = mass;
+		let total = 0;
+
+		do {
+			fuel = Math.max(this.getFuel(fuel), 0);
+			total += fuel;
+		}
+		while (fuel > 0)
+
+		return total;
+	}
+
 	// input = $("pre").innerHTML.trim().split("\n")
 
 	questionOne(input) {
-		return input.map(this.getFuel)
+		return input
+			.map(i => this.getFuel(i))
 			.reduce((a, c) => (a + c));
 	}
 
+	questionTwo(input) {
+		return input
+			.map(i => this.getAdditionalFuel(i))
+			.reduce((a, c) => (a + c));
+	}
 }
 
 module.exports = Solution;
--- spec/2019/d1.spec.js
+++ spec/2019/d1.spec.js
@@ -3,11 +3,15 @@ describe("Day 1", function() {
   var solution = new Solution();
 
   it("getFuel function", function() {
-  	var getFuel = solution.getFuel;
-  	
    	expect(solution.getFuel(12)).toBe(2);
    	expect(solution.getFuel(14)).toBe(2);
    	expect(solution.getFuel(1969)).toBe(654);
    	expect(solution.getFuel(100756)).toBe(33583);
+  });
+
+  it("getFuel function", function() {
+   	expect(solution.getAdditionalFuel(12)).toBe(2);
+   	expect(solution.getAdditionalFuel(1969)).toBe(966);
+   	expect(solution.getAdditionalFuel(100756)).toBe(50346);
   });
 });
